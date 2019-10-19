a2b013c9f428d7a9aa5cca27d6dafa62d4865fbb
Baptiste Cablé <baptiste.cable@gmail.com>
2019-10-19 09:16:32 +0200

    Knight Controller disable DEBUG.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public const float FLOOR_HEIGHT = 5f;
